3180ec8672770e440cc0c1e6168e4b83ae55159d
lucaszhh <lucaszarandon24@gmail.com>
2024-01-19 02:10:46 -0300

    add ButtonLink

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 type Props = {
@@ -9,30 +10,50 @@ type Props = {
 type PropsIcon = {
     description: string | React.ReactNode,
     icon: React.ReactNode,
-    onClick?: () => void
+    onClick?: () => void,
+    url?: string
+}
+
+type PropsIconLink = {
+    description: string | React.ReactNode,
+    icon: React.ReactNode,
+    onClick?: () => void,
+    url: string
 }
 
 export const Button = ({ description, onClick }: Props) => {
     return (
-        <button onClick={onClick} className='w-auto  flex items-center gap-2 bg-black text-white rounded-3xl dark:text-black dark:bg-white h-10 px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'>
-            {description}
+        <button onClick={onClick} className='w-auto  flex items-center gap-2 bg-black text-white rounded-3xl dark:text-black dark:bg-violet-bg h-10 px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'>
+            <span className='text-xs font-bold'>{description}</span>
         </button>
     )
 }
 
 export const ButtonIcon = ({ description, icon, onClick }: PropsIcon) => {
     return (
-        <button onClick={onClick} className='w-auto   flex items-center gap-2 bg-black text-white rounded-3xl dark:text-black dark:bg-white h-10 px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'>
+        <button onClick={onClick} className='w-auto flex items-center gap-2 bg-black text-white rounded-3xl dark:text-black dark:bg-blue-300 h-full px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'>
             {icon}
             <span className='text-xs font-bold'>{description}</span>
         </button>
     )
 }
 
+export const ButtonLink = ({ description, icon, onClick, url }: PropsIconLink) => {
+    return (
+        <Link href={url}>
+            <button onClick={onClick} className='w-auto   flex items-center gap-2 bg-neutral-800 text-white rounded-3xl dark:text-black dark:bg-white h-10 px-6 cursor-pointer active:scale-[1.1] transition-all duration-300'>
+                <span className='text-xs font-bold'>{description}</span>
+                {icon}
+            </button>
+        </Link>
+    )
+}
+
+//checkear aria-label
 export const ButtonSecondary = ({ description, onClick }: Props) => {
     return (
-        <button onClick={onClick} className='flex items-center border-2 font-medium bg-white px-3 py-2 rounded-lg text-black gap-2 active:scale-[1.1] transition-transform duration-300'>
-            {description}
+        <button aria-label='description' onClick={onClick} className='flex items-center border-2 font-medium bg-white px-3 py-2 rounded-lg text-black gap-2 active:scale-[1.1] transition-transform duration-300'>
+            <span className='text-xs font-bold'>{description}</span>
         </button>
     )
 }
