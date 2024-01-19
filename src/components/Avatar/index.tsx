60286259f0fa3824a08cd1bf261c3a9c56fb29ae
lucaszhh <lucaszarandon24@gmail.com>
2024-01-19 02:11:54 -0300

    change size avatar

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,6 +1,6 @@
 const Avatar = () => {
     return (
-        <div className="w-7 h-7 bg-black flex items-center justify-center text-white dark:bg-white dark:text-black p-5 rounded-lg">
+        <div className="w-11 h-11 bg-black flex items-center justify-center text-white dark:bg-white dark:text-black p-5 rounded-lg">
             <h1 className="font-bold">LZ</h1>
         </div>
     )
