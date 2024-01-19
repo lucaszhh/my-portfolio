8fd63baafc5fe72694803289d8dfde8b167f9224
lucaszhh <lucaszarandon24@gmail.com>
2024-01-19 02:09:13 -0300

    add icon Mail change size Dowload

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -28,8 +28,8 @@ export const Linkedin = () => {
 export const Download = () => {
     return (
         <svg xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="24"
+            height="24"
             viewBox="0 0 24 24"
             stroke-width="2" stroke="currentColor"
             fill="none" stroke-linecap="round"
@@ -111,6 +111,24 @@ export const ArrowRight = () => {
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M5 12l14 0" /><path d="M13 18l6 -6" />
             <path d="M13 6l6 6" />
+        </svg>
+    )
+}
+
+export const Mail = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"
+            width="28"
+            height="28"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+            <path d="M16 12v1.5a2.5 2.5 0 0 0 5 0v-1.5a9 9 0 1 0 -5.5 8.28" />
         </svg>
     )
 }
